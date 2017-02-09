2edf1fae791de13e37e9418eae6158f97bf572c0
christiansiegel <christiansiegel@users.noreply.github.com>
2017-02-09 16:44:16 +0100

    remove tabs

--- Src/nabto.c
+++ Src/nabto.c
@@ -65,8 +65,8 @@ static void nabto_thread(void *argument)
 }
 
 application_event_result application_event(application_request* appreq,
-		                                   unabto_query_request* r_b,
-										   unabto_query_response* w_b)
+                                           unabto_query_request* r_b,
+                                           unabto_query_response* w_b)
 {
   switch (appreq->queryId) {
   case 1: {
