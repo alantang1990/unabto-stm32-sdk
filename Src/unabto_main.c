a5f54096ac6a15962b19371492ea2ee8bc796ab1
christiansiegel <christiansiegel@users.noreply.github.com>
2017-02-24 13:53:52 +0100

    changed to AppMyProduct application

--- Src/unabto_main.c
+++ Src/unabto_main.c
@@ -12,6 +12,7 @@
 #include "unabto_main.h"
 #include "cmsis_os.h"
 #include "unabto_main.h"
+#include "demo_application.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -50,74 +51,13 @@ static void unabto_thread()
     NABTO_LOG_FATAL(("Failed at nabto_main_init"));
   }
 
+  demo_init();
+
   for (;;) {
-    osDelay(10);
     unabto_tick();
+    osDelay(10);
+    demo_application_tick();
   }
-}
-
-application_event_result application_event(application_request* appreq,
-                                           unabto_query_request* r_b,
-                                           unabto_query_response* w_b)
-{
-  switch (appreq->queryId) {
-  case 1: {
-    // <query name="light_write.json" description="Turn light on and off" id="1">
-    // <request>
-    // <parameter name="light_id" type="uint8"/>
-    // <parameter name="light_on" type="uint8"/>
-    // </request>
-    // <response>
-    // <parameter name="light_state" type="uint8"/>
-    // </response>
-    // </query>
-
-    uint8_t light_id;
-    uint8_t light_on;
-
-    // Read parameters in request
-    if (!unabto_query_read_uint8(r_b, &light_id))
-      return AER_REQ_TOO_SMALL;
-    if (!unabto_query_read_uint8(r_b, &light_on))
-      return AER_REQ_TOO_SMALL;
-
-    // Set display according to request
-    display_state = light_on;
-    if (display_state)
-      BSP_LCD_DisplayOn();
-    else
-      BSP_LCD_DisplayOff();
-
-    // Write back display state
-    if (!unabto_query_write_uint8(w_b, display_state))
-      return AER_REQ_RSP_TOO_LARGE;
-
-    return AER_REQ_RESPONSE_READY;
-  }
-  case 2: {
-    // <query name="light_read.json" description="Read light status" id="2">
-    // <request>
-    // <parameter name="light_id" type="uint8"/>
-    // </request>
-    // <response>
-    // <parameter name="light_state" type="uint8"/>
-    // </response>
-    // </query>
-
-    uint8_t light_id;
-
-    // Read parameters in request
-    if (!unabto_query_read_uint8(r_b, &light_id))
-      return AER_REQ_TOO_SMALL;
-
-    // Write back led state
-    if (!unabto_query_write_uint8(w_b, display_state))
-      return AER_REQ_RSP_TOO_LARGE;
-
-    return AER_REQ_RESPONSE_READY;
-  }
-  }
-  return AER_REQ_INV_QUERY_ID;
 }
 
 /**
